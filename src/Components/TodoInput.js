3f39ac24d26d06ad98b4ff0b33cbf79191098953
Yash97 <42500194+codeproyash@users.noreply.github.com>
2020-08-25 00:39:19 +0530

    Add files via upload

---
+++ src/Components/TodoInput.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class TodoInput extends Component {
+    render() {
+        const {item,handleChange,handleSubmit,editItem} = this.props
+        return (
+            <div className='card card-body my-3'>
+                <form onSubmit={handleSubmit}>
+                   <div className='input-group'>
+                       <div className='input-group-prepend'>
+                           <div className='input-group-text bg-primary text-white'>
+                               <i className='fas fa-book' />
+                           </div>
+                       </div>
+                       <input 
+                       type='text' 
+                       className='form-control text-capitalize' 
+                       placeholder='add todo item' 
+                       onChange={handleChange} 
+                       value={item} />
+                   </div>
+                   <button type='submit' disabled={item?false:true} className={editItem ? 'btn btn-block btn-success mt-3 text-uppercase' :
+                   'btn btn-block btn-primary mt-3 text-uppercase'   
+                }>
+                      {editItem ? "edit item" : "add item" }
+                    </button>
+                </form>
+
+            </div>
+        );
+    }
+}
+
+export default TodoInput;
